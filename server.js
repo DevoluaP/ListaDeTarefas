e42a06e1842841720c9f85f8ada1a24db840cfa7
Paulo Henrique <paulohenrique456moreira@gmail.com>
2024-10-15 21:21:16 -0300

    Adicionando arquivos para deploy no Vercel

---
+++ server.js
@@ -0,0 +1,17 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const middlewares = jsonServer.defaults();
+const fs = require("fs");
+const path = require("path");
+const data = JSON.parse(fs.readFileSync(path.join(__dirname, "db.json"), "utf-8"));
+const router = jsonServer.router(data);
+
+
+server.use(middlewares);
+
+server.use(router);
+server.listen(3000, () => {
+ console.log("JSON Server is running");
+});
+
+module.exports = server;
