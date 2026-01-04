40ef838048519ad83a7bb434a3de23039bba56ef
Paulo Henrique <devoluap@gmail.com>
2026-01-04 16:31:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const path = require("path");
 const data = JSON.parse(fs.readFileSync(path.join(__dirname, "db.json"), "utf-8"));
 const router = jsonServer.router(data);
 
-
 server.use(middlewares);
 
 server.use(router);
